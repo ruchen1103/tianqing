27f3bc7c84f7ff7e6dfc695e26cb7423cbde1a17
鲁金涛 <67625971+ruchen1103@users.noreply.github.com>
2020-08-25 15:55:49 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -98,11 +98,7 @@ function setWeatherIcon(wea) {
 //获取更新时间
 function getTime(timestamp) {
     var date = new Date(timestamp);
-    //var Y = date.getFullYear() + '-';
-    //var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
-    //var D = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate()) + ' ';
     var h = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':';
     var m = (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes());
-    // var s = (date.getSeconds() < 10 ? '0' + date.getSeconds() : date.getSeconds());
     return h + m;
 }
